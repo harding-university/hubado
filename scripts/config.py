4de9b891f85e2193ccc59ed9a60571561c6aac76
Richard Schneider <richard@schneiderbox.net>
2022-11-30 16:01:36 -0600

    Add Python config file

---
+++ scripts/config.py
@@ -0,0 +1,11 @@
+CONTEXT_APPS = {
+    'appnav': ['applicationNavigator',],
+    'accessmgmt': ['BannerAccessMgmt', 'BannerAccessMgmt.ws',],
+    'admincommon': ['BannerAdmin', 'BannerAdmin.ws',],
+    'extz': ['BannerExtensibility',],
+    'bcm': ['CommunicationManagement',],
+    'employee': ['EmployeeSelfService',],
+    'eeamc': ['ethosapimanagementcenter',],
+    'financess': ['FinanceSelfService',],
+    'general_ss': ['GeneralSelfService',],
+}
--- scripts/test_urls.py
+++ scripts/test_urls.py
@@ -5,20 +5,7 @@ import urllib
 import urllib.request
 from configparser import ConfigParser
 
-
-WEBAPPS = [
-    'applicationNavigator',
-    'BannerAccessMgmt',
-    'BannerAccessMgmt.ws',
-    'BannerAdmin',
-    'BannerAdmin.ws',
-    'BannerExtensibility',
-    'CommunicationManagement',
-    'EmployeeSelfService',
-    'ethosapimanagementcenter',
-    'FinanceSelfService',
-    'GeneralSelfService',
-]
+from config import CONTEXT_APPS
 
 
 # Makefile.local isn't exactly a .ini file like configparser epxects,
@@ -31,18 +18,19 @@ with open('Makefile.local') as makefile_local:
 banner9_root = config['Makefile']['BANNER9_ROOT']
 
 
-for webapp in WEBAPPS:
-    try:
-        res = urllib.request.urlopen(f'{banner9_root}/{webapp}/')
-        status = res.status
-        error = False
-    except urllib.error.HTTPError as e:
-        status = e.code
-        error = True
+for context, apps in CONTEXT_APPS.items():
+    for app in apps:
+        try:
+            res = urllib.request.urlopen(f'{banner9_root}/{app}/')
+            status = res.status
+            error = False
+        except urllib.error.HTTPError as e:
+            status = e.code
+            error = True
 
-    if error:
-        sys.stdout.write('\033[0;31m')
-    else:
-        sys.stdout.write('\033[0;32m')
-    print(status, webapp)
-    sys.stdout.write('\033[0;m')
+        if error:
+            sys.stdout.write('\033[0;31m')
+        else:
+            sys.stdout.write('\033[0;32m')
+        print(status, app)
+        sys.stdout.write('\033[0;m')
