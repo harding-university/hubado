f356308c7c8e5c3d0d3b23105e77026afce85c68
Richard Schneider <richard@schneiderbox.net>
2024-01-09 16:06:30 -0600

    A few small config tweaks

--- conf/banner_configuration.groovy
+++ conf/banner_configuration.groovy
@@ -5,3 +5,10 @@ bannerDataSource.password="${banner9.proxy.password}"
 bannerSsbDataSource.url="jdbc:oracle:thin:@//${oracle.host}/${oracle.service_name}"
 bannerSsbDataSource.username="${banner9.proxy.user}"
 bannerSsbDataSource.password="${banner9.proxy.password}"
+
+banner.transactionTimeout = 600
+configJob {
+    // Recommended default is every 1 hour starting at 00am, of every day - "0 0 */1 * * ?"
+    // Cron expression lesser than 30 mins will fall back to 30 mins.
+    cronExpression = "*/30 * * * *"
+}
