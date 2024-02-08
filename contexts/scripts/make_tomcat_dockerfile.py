06e54c0193cf41100574b47cfe6c19bb329c4b09
Ryan Stirrup <65615295+rstirrup@users.noreply.github.com>
2024-02-08 11:03:43 -0600

    Altered sed statements
    Rewrote the sed statements for readability, put the command for entering the worksp in, and then also made sure to preserve the whitespace.
    moved the variable declaration in make_dockerfile.py so that they could be accessed by the Dockerfile_tomcat.j2 file.

--- contexts/scripts/make_tomcat_dockerfile.py
+++ contexts/scripts/make_tomcat_dockerfile.py
@@ -11,9 +11,6 @@ from config import CONTEXT_APPS, BANNER9_ROOT, HUBADO_HOST_UID, TIMEZONE
 dockerfile_path = sys.argv[1]
 context = re.match(r"contexts/(.*)/Dockerfile", dockerfile_path).groups()[0]
 
-with open("/usr/local/share/Dockerfile_tomcat.j2") as f:
-    template = Template(f.read())
-
 app_name = CONTEXT_APPS[context][0]
 if len(CONTEXT_APPS[context]) > 1:
     worksp_name = CONTEXT_APPS[context][1]
@@ -22,6 +19,11 @@ else:
     worksp_name = ''
 hubado_host_uid = HUBADO_HOST_UID
 timezone = TIMEZONE
+
+with open("/usr/local/share/Dockerfile_tomcat.j2") as f:
+    template = Template(f.read())
+
+
 
 print(
     template.render(
